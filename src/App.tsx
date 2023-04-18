1247856d396f3fa217e8c5ecd15ee0e37a3b13f6
Victor Donizete <victordonizete65@gmail.com>
2023-04-18 13:34:47 -0300

    static routes in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { api } from "./lib/axios";
+import { useState } from "react";
+import { Products } from "./pages/products";
+import { GlobalStyles } from "./styles/global";
+import { Header } from "./pages/header";
 
-function App() {
-  const [count, setCount] = useState(0)
+// interface Product {
+//   id: number;
+//   nome: string;
+//   descricao: string;
+//   preco: number;
+//   imagem?: string;
+// }
+
+// const [nome, setNome] = useState("");
+//   const [descricao, setDescricao] = useState("");
+//   const [preco, setPreco] = useState(0);
+
+// const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+//   event.preventDefault();
+
+//   const newProduct: Product = {
+//     id: 23232,
+//     nome: nome,
+//     descricao: descricao,
+//     preco: preco
+//   };
+
+//   api.post("/products", newProduct)
+//     .then(response => {
+//       console.log(response);
+//     })
+//     .catch(error => {
+//       console.log(error);
+//     });
+//   };
+
+export function App () {
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+   <>
+    <GlobalStyles/>
+    <Header/>
+    <Products/>
+   </>
   )
 }
 
-export default App
+
