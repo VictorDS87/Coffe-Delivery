6717178802fdb1bd55ef51ca97304c8bb0636855
Victor Donizete <victordonizete65@gmail.com>
2023-04-22 19:21:18 -0300

    props

--- src/pages/header/index.tsx
+++ src/pages/header/index.tsx
@@ -1,17 +1,21 @@
 import {HeaderContainer, ShoppingCartStyle, Location} from './styles'
-import { MapPin, ShoppingCart,  } from '@phosphor-icons/react'
+import { MapPin } from '@phosphor-icons/react'
 
 import CartImg from '../../assets/header/ShoppingCart.svg'
 import logoImg from '../../assets/header/logo.svg'
 
-export function Header({ShoppingCartLength}: {ShoppingCartLength: number}) {
+interface HeaderProps {
+    childInfo: number;
+}
+
+export function Header({ childInfo }: HeaderProps) {
     return (
         <HeaderContainer>
-            <img src={logoImg} alt="" />
+            <img src={logoImg} alt="" /> 
 
             <div>
                 <Location><MapPin size={32} color='#8047F8'/><span>Porto Alegre, SP</span></Location>
-                <ShoppingCartStyle><img src={CartImg} alt="" /><span>3</span></ShoppingCartStyle>
+                <ShoppingCartStyle><img src={CartImg} alt="" /><span>{childInfo}</span></ShoppingCartStyle>
             </div>
         </HeaderContainer>
     )
