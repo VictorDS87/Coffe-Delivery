2184d970bcc9e8b03b01198484a993ddc58508fe
Victor Donizete <victordonizete65@gmail.com>
2023-05-04 20:23:24 -0300

    starting end page

---
+++ src/pages/SucessPage/index.tsx
@@ -0,0 +1,18 @@
+export function SucessPage() {
+    return (
+        <div>
+            <div>
+                <div>
+                    <h1>Uhu! Pedido confirmado</h1>
+                    <span>Agora é só agurdar que logo o café chegará até você</span>
+                </div>
+                <div>
+                    <p>1</p>
+                    <p>2</p>
+                    <p>3</p>
+                </div>
+            </div>
+            <img src="" alt="" />
+        </div>
+    )
+}
