c0205c7f70715c860075d129744a9e9d58415a21
Victor Donizete <victordonizete65@gmail.com>
2023-05-05 00:17:12 -0300

    sucess page finished

--- src/pages/SucessPage/index.tsx
+++ src/pages/SucessPage/index.tsx
@@ -1,18 +1,44 @@
+import deliveryImg from '../../assets/sucessPage/delivery.svg'
+
+import { Container, Header, OrderInformation, Summary, SummaryContainer } from "./styles";
+
+
 export function SucessPage() {
     return (
-        <div>
-            <div>
-                <div>
+        <Container>
+            <OrderInformation>
+                <Header>
                     <h1>Uhu! Pedido confirmado</h1>
                     <span>Agora é só agurdar que logo o café chegará até você</span>
-                </div>
-                <div>
-                    <p>1</p>
-                    <p>2</p>
-                    <p>3</p>
-                </div>
-            </div>
-            <img src="" alt="" />
-        </div>
+                </Header>
+                <SummaryContainer>
+                    <Summary>
+                        <div>
+                            <p>Entrega em <strong>Rua joão Daniel Martinelli, 102</strong> 
+                            <br></br>
+                            Ferrapos - Porto Alegre, RS
+                            </p>
+                        </div>
+                        <div>
+                            <p>Previsão de entrega
+                            <br></br>
+                            <strong>20 min - 30 min</strong>
+                            </p>
+                        </div>
+                        <div>
+                            <p>Forma de pagamento
+                            <br></br>
+                            <strong>Cartão de Crédito</strong>
+                            </p>
+                        </div>
+
+                    </Summary>
+                </SummaryContainer>
+            </OrderInformation>
+            <img style={{
+                width: '592px',
+                height: '431px'
+            }} src={deliveryImg} alt="" />
+        </Container>
     )
 }
--- src/pages/SucessPage/styles.ts
+++ src/pages/SucessPage/styles.ts
@@ -1,5 +1,55 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-    background: #FFFFFF;
+    display: flex;
+    justify-content: space-between;
+    margin-top: 5rem;
+`
+export const OrderInformation = styled.div`
+   
+`
+export const Header = styled.div`
+    margin-bottom: 2.199rem;
+
+    h1 {
+        font-family: 'Baloo 2', 'sans-serif';
+        font-weight: 800;
+        font-size: 2rem;
+
+        color: #C47F17;   
+
+        margin-bottom: 0;
+    }
+    
+    span {
+        font-size: 20px;
+        color: #403937;      
+    }
+   
+`
+export const SummaryContainer = styled.div`
+    border: 1px solid transparent;
+    border-radius: 6px 36px;
+
+    background: 
+        linear-gradient(to right, white, #FAFAFA), 
+        linear-gradient(to right, #DBAC2C, #8047F8); 
+    background-clip: padding-box, border-box;
+    background-origin: padding-box, border-box;
+
+    width: 526px;
+  
+  `
+export const Summary = styled.div`
+    padding: 2rem;
+
+    p{
+        font-family: 'Roboto', 'sans-serif';
+        font-weight: 400;
+        font-size: 1rem;
+    }
+
+    strong {
+        font-family: 'Roboto', 'sans-serif';
+    }
 `
